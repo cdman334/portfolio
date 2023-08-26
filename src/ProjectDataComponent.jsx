be48efc54eb4e7868c9bec2414f0a765cffb157b
cdman334 <cdman334@gmail.com>
2023-08-26 15:33:11 -0400

    Change links for projects

--- src/ProjectDataComponent.jsx
+++ src/ProjectDataComponent.jsx
@@ -7,21 +7,21 @@ function ProjectDataComponent(props) {
             "title": "Stock Exploratory Data Analysis",
             "description": "Analyze stock data to provide insights and visualizations for informed stakeholder decisions.",
             "imageUrl": props.stocks,
-            "url": "https://colab.research.google.com/drive/1i70RU-QOTbK7KVY0zbqi0F2YBiRPepuK"
+            "url": "https://github.com/cdman334/exploratory_data_analysis"
         },
         {
             "id": 2,
             "title": "Sentiment Analysis",
             "description": "Sentiment Analysis in Python, leveraging NewsAPI for data and various libraries for analysis.",
             "imageUrl": props.likes,
-            "url": "https://colab.research.google.com/drive/1f2cNf2d7K5N3sSP5jyk3jBjOVr3_w4ZC"
+            "url": "https://github.com/cdman334/sentiment_analysis"
         },
         {
             "id": 3,
             "title": "Real Estate Price Predictor",
             "description": "Predicting house prices based on various features using machine learning algorithms.",
             "imageUrl": props.realEstate,
-            "url": "https://colab.research.google.com/drive/1Q79OVM3YPF9XtM_a1KthaPt5hvkPGM5M"
+            "url": "https://github.com/cdman334/properties"
         }
     ];
 }
